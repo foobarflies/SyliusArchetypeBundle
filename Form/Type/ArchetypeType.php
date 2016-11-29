1e3670f12a15417ecbc9816df6b377f5774a0c3f
tchapi <regbasket+github@gmail.com>
2016-11-29 22:33:36 +0100

    Use FQDN for everything possible
    Introduce possible strong bond between Product and Archetype

--- Form/Type/ArchetypeType.php
+++ Form/Type/ArchetypeType.php
@@ -14,13 +14,18 @@ namespace Sylius\Bundle\ArchetypeBundle\Form\Type;
 use Sylius\Bundle\ArchetypeBundle\Form\EventListener\ParentArchetypeListener;
 use Sylius\Bundle\ResourceBundle\Form\EventSubscriber\AddCodeFormSubscriber;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Sylius\Bundle\ResourceBundle\Form\Type\ResourceTranslationsType;
+use Sylius\Bundle\ProductBundle\Form\Type\ProductOptionChoiceType;
+use Sylius\Bundle\ProductBundle\Form\Type\ProductAttributeValueType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 /**
  * Product archetype form type.
  *
  * @author Adam Elsodaney <adam.elso@gmail.com>
  * @author Paweł Jędrzejewski <pawel@sylius.org>
+ * @author Cyril Chapellier <tchap@tchap.me>
  */
 class ArchetypeType extends AbstractResourceType
 {
@@ -49,16 +54,17 @@ class ArchetypeType extends AbstractResourceType
         $builder
             ->addEventSubscriber(new ParentArchetypeListener($this->subject))
             ->addEventSubscriber(new AddCodeFormSubscriber())
-            ->add('translations', 'sylius_translations', [
-                'type' => sprintf('sylius_%s_archetype_translation', $this->subject),
+            ->add('translations', ResourceTranslationsType::class, [
+                'type' => ArchetypeTranslationType::class,
                 'label' => 'sylius.form.archetype.name',
             ])
-            ->add('attributes', sprintf('sylius_%s_attribute_choice', $this->subject), [
+            ->add('attributes', CollectionType::class, [
+                'entry_type' => ProductAttributeValueType::class,
                 'required' => false,
                 'multiple' => true,
                 'label' => 'sylius.form.archetype.attributes',
             ])
-            ->add('options', sprintf('sylius_%s_option_choice', $this->subject), [
+            ->add('options', ProductOptionChoiceType::class, [
                 'required' => false,
                 'multiple' => true,
                 'label' => 'sylius.form.archetype.options',
